246287f4a70c7b26390c0f84964a2e72532126f4
andream16 <andrea.medda.uni@gmail.com>
2018-06-19 12:13:48 +0200

    Adding cmd/aws/sns/util

---
+++ cmd/aws/sns/util.go
@@ -0,0 +1,26 @@
+package sns
+
+import (
+	"strings"
+	"encoding/json"
+)
+
+// UnmarshalMessage unmarshal an SNS Message to a given interface
+func UnmarshalMessage(msg string, in interface{}) error {
+
+	uS := unescapeMessageString(msg)
+
+	unmarshalErr := json.Unmarshal([]byte(uS), in)
+	if unmarshalErr != nil {
+		return unmarshalErr
+	}
+
+	return nil
+
+}
+
+// unescapeMessageString takes a SNS message string like
+// `"{\"stuff\" : \"somevalue\"}"` and outputs `"{"stuff" : "somevalue"}"`
+func unescapeMessageString(in string) string {
+	return strings.Replace(in, `\"`, `"`, -1)
+}
---
+++ cmd/aws/sns/util_test.go
@@ -0,0 +1,34 @@
+package sns
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+type TestSNSMessage struct {
+	Param1 string `json:"param1"`
+}
+
+func TestMessageToJson(t *testing.T) {
+
+	var p TestSNSMessage
+
+	str := `{\"param1\":\"some_value\"}`
+
+	err := UnmarshalMessage(str, &p)
+
+	assert.NoError(t, err)
+	assert.Equal(t, "some_value", p.Param1)
+
+}
+
+func TestUnescapeMessageString(t *testing.T) {
+
+	str := `{\"some_param\":\"some_value\"}`
+
+	s := unescapeMessageString(str)
+
+	assert.Equal(t, `{"some_param":"some_value"}`, s)
+
+}
+
